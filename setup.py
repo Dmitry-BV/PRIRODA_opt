c07015f825bffa2cbdc3ea9ad6032c9a6cb88d48
Dmitry-BV <69424429+Dmitry-BV@users.noreply.github.com>
2021-04-29 18:55:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='conformer',
     version='0.1.0',
+    license='CIMM.07-013',
+    author="Dmitry x2, Zhenya, P'yanoe Hujlo, Darya, Polina, Il'nura, Roma, Anya",
     packages=['PrirodaOptimiser'],
     zip_safe=False,
 )
